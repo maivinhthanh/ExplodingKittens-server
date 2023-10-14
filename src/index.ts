a0e5d2d6fd1e82d9cc088e6ac31718166f6839b5
Mai Vinh Thanh <maivinhthanh@Mai-Vinh-Thanh-Mac-Pro.local>
2023-10-14 15:51:07 +0700

    socket

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,54 @@
-import express, { NextFunction, Request, Response } from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import userRoutes from './routes/user';
-import * as database from './database';
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+
+import userRoutes from "./routes/user";
+import * as database from "./database";
 
 const app = express();
+const server = createServer(app);
 const port = process.env.PORT || 3000;
+const portSocket = 4000;
 
 app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, TypeScript Express!");
 });
 
 const init = async () => {
   await database.init();
-}
+};
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
-  res.status(500).send('Something went wrong');
+  res.status(500).send("Something went wrong");
 });
 
+const io = new Server({
+  cors: {
+    origin: "http://127.0.0.1:5173",
+  },
+});
 
-app.listen(port, () => {
+io.listen(portSocket);
+
+server.listen(port, () => {
   init();
   console.log(`Server running at http://localhost:${port}`);
 });
